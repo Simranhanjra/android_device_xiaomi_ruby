fb3ccb21edb910e3c2dcac0e1bcf817386cff1be
bengris32 <bengris32@protonmail.ch>
2024-03-01 19:22:29 +0000

    ruby: Build common MediaTek Gadget HAL
    Change-Id: I4951a30cee215aab57ce44ab86ef9ee2b60fb360
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -406,7 +406,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 
 # Vibrator
 $(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
